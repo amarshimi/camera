e3d676f735f273cf31be4db9a25e11ffe6082807
Elkana Hoizman <elkana058@gmail.com>
2020-01-06 23:18:04 +0200

    add some design

---
+++ js/script.js
@@ -0,0 +1,28 @@
+var video = document.getElementById('video');
+
+// Get access to the camera!
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    // Not adding `{ audio: true }` since we only want video now
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+}
+
+// Elements for taking the snapshot
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+var video = document.getElementById('video');
+
+// Trigger photo take
+document.getElementById("snap").addEventListener("click", function () {
+    context.drawImage(video, 0, 0, 640, 480);
+    to_image();
+});
+
+function to_image() {
+    var canvas = document.getElementById("canvas");
+    document.getElementById("theimage").src = canvas.toDataURL();
+    // Canvas2Image.saveAsPNG(canvas);
+}
